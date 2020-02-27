3220bfcbde842c990fa9f264daeda683cbfff32d
demetri selmer <demetriselmer@demetris-MacBook-Air.local>
2020-02-26 23:24:40 -0600

    almost finsehed with sign in form need to add more styling

--- disneyparent/src/Components/SignInForm.js
+++ disneyparent/src/Components/SignInForm.js
@@ -1,10 +1,50 @@
 import React from 'react';
-import {Form,Field,}
+import {withFormik,Form,Field,} from 'formik';
+import { Button,  FormGroup, Label, Input, FormText } from 'reactstrap';
 
-const SignInForm=()=>{
+const SignInForm=(props)=>{
+    console.log('props.values from sign in form',props.values)
     return (
+        <Form>
+    <FormGroup>
+            <label>
+                Name:
+                <Field type='text' name='name' placeholder='Name'/>
+            </label>
+    </FormGroup>
+    <FormGroup>
+            <label>
+                   E-mail:
+                   <Field type='text' name = 'email' placeholder='E-mail'/>
+            </label>
+    </FormGroup>
+    <FormGroup>
+            <label>
+                   Password:
+                   <Field type='text' name = 'password' placeholder='Password'/>
+            </label>
+    </FormGroup>
+    <FormGroup>
+    
+            <label>
+                   Terms of Service:
+                    <Field type='checkbox' name = 'termsOfService' />
+            </label>
+        </FormGroup>
+      <Button color="info" type ='submit'>Submit</Button>
+    </Form>
        
     )
 }
+ 
 
-export default SignInForm
+export default withFormik({
+    mapPropsToValues:()=>({
+      name:'',
+      email:'',
+      password:'',
+      termsOfService:false
+    }),
+  
+  
+  }) (SignInForm);
