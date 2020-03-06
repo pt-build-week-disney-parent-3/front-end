d7f028eb2888e8944079e11b929dbf0b952b44f1
dsh410 <dsh410@gmail.com>
2020-03-05 23:40:40 -0600

    finshed search from

--- disneyparent/src/Components/SearchForm.js
+++ disneyparent/src/Components/SearchForm.js
@@ -8,29 +8,44 @@ import {NavLink} from 'react-router-dom';
 const SearchForm = (props)=>{
    const [searchTerm ,setSearchTerm] = useState('')
    const [results ,setResults] = useState([])
+   const [questions,setQuestions] = useState([])
+
+   
    const handleChanges = (e) => {
      setSearchTerm(e.target.value);
    }
 
     useEffect(() => {
       axiosWithAuth()
-      .get('/questions')
+      .get('/api/questions')
       .then(response =>{
         console.log('this is the response',response)
+        setQuestions(response.data)
       })
       .catch(err=>{console.log('this is a error',err)})
 
-    // const results = people.filter(person =>
-    //   person.toLowerCase().includes(searchTerm)
-    // );
-    // setSearchResults(results);
-  }, [searchTerm]);
+  
+      
+    }, []);
+
+    console.log('this is questions',questions)
+
+    useEffect(()=>{
+
+    const output = questions.filter(question =>
+        question.question.toLowerCase().includes(searchTerm)
+      );
+      setResults(output);
+
+    },[searchTerm])
 
 
 
     return (
     
         <div>
+
+          
           <NavLink  to="/">
             <p style={{marginLeft:'2em',marginTop:'1em' }} >Home</p>
           </NavLink>
@@ -44,12 +59,17 @@ const SearchForm = (props)=>{
                   name="search"
                   value={searchTerm}
                   onChange={handleChanges}
-                  
                   placeholder="Questions"
                 />
                 <Button color="info" type ='submit' style={{ marginLeft:'55em',marginTop:'1em',color:'white'}}>Search</Button>
               </FormGroup>
           </Form>
+
+          
+          {results.map(result=>{
+            console.log('result',result)
+            return <p>{result.question}</p>
+          })}
         </div>
       
     )
