308a2abdf453b60d8c9259a3657cfb726378a7d7
alexis-anderson <aanderson9313@gmail.com>
2020-03-08 22:27:33 -0400

    fixed search-issues

--- disneyparent/src/Components/SearchForm.js
+++ disneyparent/src/Components/SearchForm.js
@@ -1,19 +1,28 @@
 import React,{ useState,useEffect} from 'react';
 import { axiosWithAuth } from '../auth/axiosWithAuth';
-
-import { Button,  FormGroup, Label,Input,Form} from 'reactstrap';
+import { Button,  FormGroup, Label,Input,Form,} from 'reactstrap';
 import styled from 'styled-components';
 // import {NavLink} from 'react-router-dom';
 
 
 const Disclaimer = styled.small`
-margin-left: 20%;
+margin-left: 2%;
 color: red;
-
+text-align: justify;
+align-content: left;
+width: 20%;
 `
 
 const Container = styled.div`
 background-color: #EEEEEE;
+`
+
+const Time = styled.div `
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-content: space-evenly;
+width: 100%;
 `
 const SearchForm = (props)=>{
    const [searchTerm ,setSearchTerm] = useState('')
@@ -53,41 +62,39 @@ const SearchForm = (props)=>{
 
     return (
     
-        <div>
-
-          
-          <NavLink  to="/">
-            <p style={{marginLeft:'2em',marginTop:'1em' }} >Home</p>
-          </NavLink>
+        <Container>
         
           <Form>
             <div>
               <FormGroup>
                 <Label  style={{marginLeft:'2em' }} for="Search">Search:</Label>
                 <Input
-                style={{marginLeft:'2em' ,width:'60em'}}
+                style={{marginLeft:'2em' ,width:'90%'}}
                   type="text"
                   name="search"
                   value={searchTerm}
                   onChange={handleChanges}
                   placeholder="Questions"
                 />
-                <Label style = {{marginLeft: '2em'}} for = 'Meeting Location'>Meeting Location:</Label>
-                <Field 
-                style = {{marginLeft:'2em', width:'30em'}}
-                component = 'select'
+                <Time>
+                <Label style = {{width: '15%'}} for = 'Meeting Location'>Meeting Location:</Label>
+                <Input 
+                style = {{width:'20%'}}
+                type = 'select'
                 name = 'meeting location'
                 >
                   <option value = 'Tomorrow-Land'>Tomorrowland</option>
                   <option value = 'Liberty-Square'>LibertySquare</option>
                   <option value = 'Adventure-Land'>Adventureland</option>
                   <option value = 'Frontier-Land'>Frontierland</option>
-                </Field>
-                <Label style = {{marginRight: '2em', paddingLeft: '20px'}} for = 'Time'>Time:</Label>
-                <input type = 'time' name = 'time'  >
-                  
-                </input><br/>
-                <Disclaimer >*For the safety of all of our guests, meetup times must be between 7am and 9pm </Disclaimer>
+                </Input>
+               
+                  <label style = {{ width: '10%'}}for = 'Time'>Time:</label>
+                  <Input type = 'time' name = 'time' style = {{ width: '15em'}}  >
+                    
+                  </Input><br/>
+                  <Disclaimer >*For the safety of all of our guests, meetup times must be between 7am and 9pm </Disclaimer>
+                </Time>
                 <Button color="info" type ='submit' style={{ marginLeft:'55em',marginTop:'1em',color:'white'}}>Search</Button>
               </FormGroup>
               </div>
@@ -98,7 +105,7 @@ const SearchForm = (props)=>{
             console.log('result',result)
             return <p>{result.question}</p>
           })}
-        </div>
+        </Container>
       
     )
 }
